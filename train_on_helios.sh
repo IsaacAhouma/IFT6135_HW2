62cb7eab0674b8618587d9643f3b89c7687474f0
IsaacAhouma <ivorianboy@gmail.com>
2019-03-23 11:03:32 -0400

    try rnn experiment for 4.1

--- train_on_helios.sh
+++ train_on_helios.sh
@@ -5,8 +5,7 @@
 #PBS -l nodes=1:gpus=1
 #PBS -l walltime=10:00:00
 
-s_exec python 'ptb-lm.py' --model=RNN --optimizer=SGD --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35
-
+s_exec python 'ptb-lm.py' --model=RNN --optimizer=ADAM --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_best
 # echo "Copying files to local hard drive..."
 # cp -r $TMP_RESULTS_DIR $ROOT_DIR
 # echo "Cleaning up data and results..."
