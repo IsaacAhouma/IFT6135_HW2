9a803d877445f6ba61a6b602fe5743072b9d97f3
IsaacAhouma <ivorianboy@gmail.com>
2019-03-23 08:44:38 -0400

    update initializations

---
+++ train_on_helios.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#PBS -A colosse-users
+#PBS -l feature=k80
+#PBS -l nodes=1:gpus=1
+#PBS -l walltime=10:00:00
+
+
+s_exec python 'ptb-lm.py' --model=RNN --optimizer=SGD_LR_SCHEDULE --initial_lr=1 --batch_size=20 --seq_len=35 --hidden_size=512 --num_layers=2 --dp_keep_prob=0.35
+
+# echo "Copying files to local hard drive..."
+# cp -r $TMP_RESULTS_DIR $ROOT_DIR
+
+# echo "Cleaning up data and results..."
+# rm -r $TMP_DATA_DIR
+# rm -r $TMP_RESULTS_DIR
