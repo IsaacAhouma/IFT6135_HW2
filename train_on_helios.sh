11e11c0ac475962835d45104fbe119858faf379c
IsaacAhouma <ivorianboy@gmail.com>
2019-03-23 22:44:28 -0400

    add plotting function

--- train_on_helios.sh
+++ train_on_helios.sh
@@ -5,7 +5,7 @@
 #PBS -l nodes=1:gpus=1
 #PBS -l walltime=10:00:00
 
-s_exec python 'ptb-lm.py' --model=GRU --optimizer=SGD_LR_SCHEDULE --initial_lr=10 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_best
+s_exec python 'ptb-lm.py' --model=TRANSFORMER --optimizer=SGD_LR_SCHEDULE --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=0.9 --save_best
 # echo "Copying files to local hard drive..."
 # cp -r $TMP_RESULTS_DIR $ROOT_DIR
 # echo "Cleaning up data and results..."
