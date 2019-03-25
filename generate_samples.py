dc8fc11c39bfde2ddf8001aca5ab0884a87bb392
milongo <emilio.botero@umontreal.ca>
2019-03-25 15:58:10 -0400

    generate samples code

--- generate_samples.py
+++ generate_samples.py
@@ -1,7 +1,189 @@
 import models
 import os
 import torch
+import numpy as np
+import collections
+from torch.autograd import Variable
+import argparse
+from models import RNN, GRU
+parser = argparse.ArgumentParser(description='PyTorch Penn Treebank Language Modeling')
 
-cwd = os.getcwd()
-path_to_rnn = cwd + '/best_params_rnn.pt'
-path_to_gru = cwd + '/best_params_gru.pt'
+# Arguments you may need to set to run different experiments in 4.1 & 4.2.
+parser.add_argument('--data', type=str, default='data',
+                    help='location of the data corpus. We suggest you change the default\
+                    here, rather than passing as an argument, to avoid long file paths.')
+parser.add_argument('--model', type=str, default='RNN',
+                    help='type of recurrent net (RNN, GRU, TRANSFORMER)')
+parser.add_argument('--optimizer', type=str, default='SGD_LR_SCHEDULE',
+                    help='optimization algo to use; SGD, SGD_LR_SCHEDULE, ADAM')
+parser.add_argument('--seq_len', type=int, default=35,
+                    help='number of timesteps over which BPTT is performed')
+parser.add_argument('--batch_size', type=int, default=10,
+                    help='size of one minibatch')
+parser.add_argument('--initial_lr', type=float, default=20.0,
+                    help='initial learning rate')
+parser.add_argument('--hidden_size', type=int, default=200,
+                    help='size of hidden layers. IMPORTANT: for the transformer\
+                    this must be a multiple of 16.')
+parser.add_argument('--save_best', action='store_true',
+                    help='save the model for the best validation performance')
+parser.add_argument('--num_layers', type=int, default=2,
+                    help='number of hidden layers in RNN/GRU, or number of transformer blocks in TRANSFORMER')
+
+# Other hyperparameters you may want to tune in your exploration
+parser.add_argument('--emb_size', type=int, default=200,
+                    help='size of word embeddings')
+parser.add_argument('--num_epochs', type=int, default=40,
+                    help='number of epochs to stop after')
+parser.add_argument('--dp_keep_prob', type=float, default=0.35,
+                    help='dropout *keep* probability. drop_prob = 1-dp_keep_prob \
+                    (dp_keep_prob=1 means no dropout)')
+
+# Arguments that you may want to make use of / implement more code for
+parser.add_argument('--debug', action='store_true')
+parser.add_argument('--save_dir', type=str, default='',
+                    help='path to save the experimental config, logs, model \
+                    This is automatically generated based on the command line \
+                    arguments you pass and only needs to be set if you want a \
+                    custom dir name')
+parser.add_argument('--evaluate', action='store_true',
+                    help="use this flag to run on the test set. Only do this \
+                    ONCE for each model setting, and only after you've \
+                    completed ALL hyperparameter tuning on the validation set.\
+                    Note we are not requiring you to do this.")
+parser.add_argument('--load_weights', default=None, type=str)
+
+# DO NOT CHANGE THIS (setting the random seed makes experiments deterministic,
+# which helps for reproducibility)
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+
+args = parser.parse_args()
+
+# HELPER FUNCTIONS
+def _read_words(filename):
+    with open(filename, "r") as f:
+      return f.read().replace("\n", "<eos>").split()
+
+
+def _build_vocab(filename):
+    data = _read_words(filename)
+    counter = collections.Counter(data)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+    id_to_word = dict((v, k) for k, v in word_to_id.items())
+    return word_to_id, id_to_word
+
+
+def _file_to_word_ids(filename, word_to_id):
+    data = _read_words(filename)
+    return [word_to_id[word] for word in data if word in word_to_id]
+
+
+# Processes the raw data from text files
+def ptb_raw_data(data_path=None, prefix="ptb"):
+    train_path = os.path.join(data_path, prefix + ".train.txt")
+    valid_path = os.path.join(data_path, prefix + ".valid.txt")
+    test_path = os.path.join(data_path, prefix + ".test.txt")
+    # Hmm
+    word_to_id, id_2_word = _build_vocab(train_path)
+    train_data = _file_to_word_ids(train_path, word_to_id)
+    valid_data = _file_to_word_ids(valid_path, word_to_id)
+    test_data = _file_to_word_ids(test_path, word_to_id)
+    return train_data, valid_data, test_data, word_to_id, id_2_word
+
+
+# Yields minibatches of data
+def ptb_iterator(raw_data, batch_size, num_steps):
+    raw_data = np.array(raw_data, dtype=np.int32)
+    data_len = len(raw_data)
+    batch_len = data_len // batch_size
+    data = np.zeros([batch_size, batch_len], dtype=np.int32)
+    for i in range(batch_size):
+        data[i] = raw_data[batch_len * i:batch_len * (i + 1)]
+    # Hmm
+    epoch_size = (batch_len - 1) // num_steps
+    # Hmm
+    if epoch_size == 0:
+        raise ValueError("epoch_size == 0, decrease batch_size or num_steps")
+    # Hmm
+    for i in range(epoch_size):
+        x = data[:, i*num_steps:(i+1)*num_steps]
+        y = data[:, i*num_steps+1:(i+1)*num_steps+1]
+        yield (x, y)
+
+
+class Batch:
+    "Data processing for the transformer. This class adds a mask to the data."
+    def __init__(self, x, pad=-1):
+        self.data = x
+        self.mask = self.make_mask(self.data, pad)
+    @staticmethod
+    def make_mask(data, pad):
+        "Create a mask to hide future words."
+        def subsequent_mask(size):
+            """ helper function for creating the masks. """
+            attn_shape = (1, size, size)
+            subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+            return torch.from_numpy(subsequent_mask) == 0
+        mask = (data != pad).unsqueeze(-2)
+        mask = mask & Variable(
+            subsequent_mask(data.size(-1)).type_as(mask.data))
+        return mask
+
+
+if torch.cuda.is_available():
+    print("Using the GPU")
+    device = torch.device("cuda")
+else:
+    print("WARNING: You are about to run on cpu, and this will likely run out \
+      of memory. \n You can try setting batch_size=1 to reduce memory usage")
+    device = torch.device("cpu")
+
+
+# LOAD DATA
+print('Loading data from '+'data')
+raw_data = ptb_raw_data(data_path='data')
+train_data, valid_data, test_data, word_to_id, id_2_word = raw_data
+vocab_size = len(word_to_id)
+print('  vocabulary size: {}'.format(vocab_size))
+
+
+if args.model == 'RNN':
+    model = RNN(emb_size=args.emb_size, hidden_size=args.hidden_size,
+                seq_len=args.seq_len, batch_size=args.batch_size,
+                vocab_size=vocab_size, num_layers=args.num_layers,
+                dp_keep_prob=args.dp_keep_prob)
+elif args.model == 'GRU':
+    model = GRU(emb_size=args.emb_size, hidden_size=args.hidden_size,
+                seq_len=args.seq_len, batch_size=args.batch_size,
+                vocab_size=vocab_size, num_layers=args.num_layers,
+                dp_keep_prob=args.dp_keep_prob)
+
+if args.load_weights is not None and os.path.isfile(args.load_weights):
+    model.load_state_dict(torch.load(args.load_weights, map_location=device))
+
+
+def generate(model, generated_seq_len, num_of_samples):
+    """
+    One epoch of training/validation (depending on flag is_train).
+    """
+
+    x = np.random.choice(vocab_size, (1, num_of_samples))
+    inputs = torch.from_numpy(x.astype(np.int64)).transpose(0, 1).contiguous().to('cpu')
+
+    model.eval()
+    model.zero_grad()
+    hidden = model.init_hidden().to('cpu')
+    samples = model.generate(inputs, hidden, generated_seq_len-1) # (seq_len, batch_size)
+    sample_words = [' '.join([id_2_word[t] for t in seq]) for seq in samples.detach().cpu().numpy().T]
+    return sample_words
+
+
+samples1 = generate(model, 35, args.batch_size)
+samples2 = generate(model, 70, args.batch_size)
+
+np.save('generated_gru.npy', samples1, samples2)
+
+
